373a09f0ed33523dd573abe928694b8b86f5fb14
zsarge <zackarywsargent@gmail.com>
2021-07-17 21:01:46 -0400

    format numbers as X's and O's

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 // I'm making a tic-tac-toe game to learn rust
 
+// TO DO:
+// Consider making union type of " ", "X", and "O" ?
+
 struct Board {
     // a 3x3 board
     board: [[u8; 3]; 3],
+}
+
+fn format(n: &u8) -> String {
+    match n {
+        0 => " ",
+        1 => "X",
+        2 => "O",
+        _ => panic!("not valid")
+    }.to_string()
 }
 
 impl Board {
@@ -10,7 +22,7 @@ impl Board {
         for row in self.board.iter() {
             print!("[");
             for (idx, val) in row.iter().enumerate() {
-                print!("{}", val);
+                print!("{}", format(val));
                 if idx < row.len() - 1 {
                     print!(", ");
                 }
