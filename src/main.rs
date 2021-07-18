d586eb7b5f8c8ad35ff6e31179321f087cbbed34
zsarge <zackarywsargent@gmail.com>
2021-07-17 23:01:04 -0400

    add structure for turns

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // I'm making a tic-tac-toe game to learn rust
 
 use std::fmt;
+use std::io;
+use std::io::*;
 
 // A square in a tic-tac-toe board
 enum Square {
@@ -20,13 +22,40 @@ impl fmt::Display for Square {
     }
 }
 
+enum Turn {
+    X,
+    O,
+}
+
+impl fmt::Display for Turn {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let printable = match *self {
+            Turn::X => 'X',
+            Turn::O => 'O',
+        };
+        write!(f, "{}", printable)
+    }
+}
+
 struct Board {
     // a 3x3 board
     board: [[Square; 3]; 3],
+    won: bool,
+    turn: Turn,
+}
+
+fn take_input() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("error: unable to read user input");
+    println!("DEBUG: {}", input);
+    input
 }
 
 impl Board {
     fn print(&self) {
+        println!("");
         for (index, row) in self.board.iter().enumerate() {
             println!(" {0} | {1} | {2} ", row[0], row[1], row[2]);
 
@@ -34,6 +63,14 @@ impl Board {
                 println!("---+---+---");
             }
         }
+        println!("");
+    }
+
+    fn prompt(&self) {
+        Board::print(self);
+
+        println!("- {} move: ", self.turn);
+        let input = take_input();
     }
 }
 
@@ -44,6 +81,9 @@ fn main() {
             [Square::Value(4), Square::Value(5), Square::Value(6)],
             [Square::Value(7), Square::Value(8), Square::Value(9)],
         ],
+        won: false,
+        turn: Turn::X,
     };
-    Board::print(&b);
+
+    Board::prompt(&b);
 }
