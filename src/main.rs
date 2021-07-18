d8c57c8498de42a44991f0788eaacff04468f837
zsarge <zackarywsargent@gmail.com>
2021-07-17 20:43:30 -0400

    add basic board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+// I'm making a tic-tac-toe game to learn rust
+
+struct Board {
+    // a 3x3 board
+    board: [[u8; 3]; 3],
+}
+
+impl Board {
+    fn print(&self) {
+        for i in self.board.iter() {
+            print!("[");
+            for (idx, val) in i.iter().enumerate() {
+                print!("{}", val);
+                if idx < i.len() - 1 {
+                    print!(", ");
+                }
+            }
+            println!("]");
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let b = Board {
+        board: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    };
+    Board::print(&b);
 }
